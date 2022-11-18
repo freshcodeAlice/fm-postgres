f97984b427833ee825c4fc359f44935437cfe068
freshcodeAlice <freshjs.ofte@gmail.com>
2022-11-18 10:11:40 +0200

    creating table

--- postgres-test.session.sql
+++ postgres-test.session.sql
@@ -1,1 +1,19 @@
-CREATE TABLE test();
+
+DROP TABLE users;
+
+
+CREATE TABLE users(
+    first_name varchar(64),
+    last_name varchar(64),
+    email text
+);
+
+
+/*
+char(5):'1234567' -> error, '12' -> '12   '
+varchar(5): '1234567' -> error, '12' -> '12'
+
+text -> max 1GB
+
+
+*/
