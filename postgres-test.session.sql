41d61bee9aa6b9c12d8125d47b82edc3af139fd0
freshcodeAlice <freshjs.ofte@gmail.com>
2022-11-21 12:04:20 +0200

    books task resolve

--- postgres-test.session.sql
+++ postgres-test.session.sql
@@ -66,3 +66,76 @@ INSERT INTO a VALUES
 (2,3);
 
 INSERT INTO a VALUES (2,2);
+
+
+/* ALTER */
+DROP TABLE products;
+
+CREATE TABLE products (
+    id serial PRIMARY KEY,
+    brand varchar(200) NOT NULL,
+    model varchar(300) NOT NULL,
+    description text,
+    category varchar(200) NOT NULL,
+    price numeric(10,2) NOT NULL CHECK (price > 0),
+    discounted_price numeric(10,2) CHECK (discounted_price <= price)
+);
+
+INSERT INTO products (brand, model, category, price) VALUES 
+('Samsung', 'S10', 'smartphones', 200),
+('iPhone', '5', 'smartphones', 500),
+('Realme', '6', 'smartphones', 100),
+('LG', '5', 'tv', 300),
+('Sony', '45', 'tv', 1000);
+
+
+INSERT INTO products (brand, model, category, price) VALUES 
+('Samsung', 'S11', 'smartphones', 200),
+('iPhone', '6', 'smartphones', 500);
+
+
+ALTER TABLE products
+ADD COLUMN quantity int CHECK (quantity > 0);
+
+
+ALTER TABLE products
+ADD CONSTRAINT "unique_brand_model_pair" UNIQUE(brand, model);
+
+
+ALTER TABLE products 
+DROP CONSTRAINT "products_quantity_check";
+
+ALTER TABLE products
+ADD CONSTRAINT "products_quantity_check" CHECK (quantity >= 0);
+
+
+/*
+ ALTER TABLE products
+ ALTER COLUMN quantity SET NOT NULL 
+
+Не можемо оновити дані і встановити констрейн - дані в таблиці містять NULL
+
+ */
+
+
+/* Books task */
+
+
+CREATE TABLE books (
+    id serial PRIMARY KEY,
+    author varchar(256),
+    name varchar(256),
+    year varchar(4),
+    publisher varchar(256),
+    category varchar(256),
+    synopsys text,
+    quantity int,
+    status varchar(100)
+);
+
+ALTER TABLE books
+ADD CONSTRAINT "quantity_more_zero" CHECK (quantity >= 0);
+
+ALTER TABLE books
+ADD CONSTRAINT "author_name_unique" CHECK (author != '' AND name != '') UNIQUE (author, name);
+
