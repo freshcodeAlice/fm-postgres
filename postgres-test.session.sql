b0e5503823db7baef1a5d87712054bf755bcc6e2
freshcodeAlice <freshjs.ofte@gmail.com>
2022-11-22 11:59:08 +0200

    chats to users task resolve

--- postgres-test.session.sql
+++ postgres-test.session.sql
@@ -160,3 +160,37 @@ CREATE TABLE orders_to_products(
     quantity int,
     PRIMARY KEY(order_id, product_id)
 );
+
+
+--////////--
+
+/* Реалізуйте чат між юзерами. 
+В кожного чата є певний власник (owner).
+В кожного юзера може бути багато чатів. В одному чаті - багато юзерів.
+В кожному чаті - багато повідомлень. Одне повідомлення - в одному чаті.  */
+
+
+CREATE TABLE chats(
+    id serial PRIMARY KEY,
+    name varchar(256) NOT NULL,
+    owner_id int REFERENCES users(id),
+    created_at timestamp DEFAULT current_timestamp
+);
+
+CREATE TABLE chats_to_users(
+    chat_id int REFERENCES chats(id),
+    user_id int REFERENCES users(id),
+    join_at timestamp DEFAULT current_timestamp,
+    PRIMARY KEY(chat_id, user_id)
+);
+
+
+CREATE TABLE messages(
+    id serial PRIMARY KEY,
+    body text NOT NULL CHECK (body != ''),
+    created_at timestamp DEFAULT current_timestamp,
+    isRead boolean NOT NULL,
+    author_id int,
+    chat_id int,
+    FOREIGN KEY (author_id, chat_id) REFERENCES chats_to_users (chat_id, user_id)
+)
