b27e93497249f1be104e1152760f734d952986b1
freshcodeAlice <freshjs.ofte@gmail.com>
2022-11-28 11:57:03 +0200

    Tasks resolve

--- select.sql
+++ select.sql
@@ -145,3 +145,86 @@ RETURNING *;
 
 SELECT * FROM users
 WHERE id = 2541;
+
+
+----------------
+
+
+SELECT id, first_name, last_name, birthday, extract('years' from birthday) FROM users
+WHERE id = 5;
+
+
+
+/*
+1. Отримати всіх повнолітніх користувачів чоловічого роду.
+
+2. Отримати всіх користувачів-жінок, ім'я яких починається на "А"
+
+3. Отримати всіх користувачів, вік яких від 20 до 40 років
+
+4. Отримати всіх користувачів, які народились у вересні
+SELECT * FROM users
+WHERE extract('month' from birthday) = 9;
+
+
+5. Всім користувачам, які народились 1 листопада, змінити підписку на true
+
+6. Видалити всіх користувачів, які старші за 65 років.
+
+7. Всім користувачам чоловічого роду віком від 40 до 50 встановити вагу = 80;
+
+
+*/
+
+
+
+---1
+
+SELECT * FROM users
+WHERE gender = 'male' AND extract ('years' from age(birthday)) > 18; --v1
+
+SELECT * FROM users
+WHERE gender = 'male' AND extract ('years' from birthday) < 2004;
+
+--- 2
+
+SELECT * FROM users
+WHERE gender = 'female' AND first_name LIKE 'A%';
+
+---- 3
+
+SELECT * FROM users
+WHERE extract('years' from age(birthday)) BETWEEN 20 AND 40; ---v1 ----best
+
+SELECT * FROM users
+WHERE age(birthday) BETWEEN make_interval(20) AND make_interval(40); --v2
+
+
+----4
+
+SELECT * FROM users
+WHERE extract('month' from birthday) = 9;
+
+
+----5
+
+UPDATE users
+SET is_subscribe = true
+WHERE extract('month' from birthday) = 12 AND extract('day' from birthday) = 1;
+
+
+SELECT * FROM users
+WHERE extract('month' from birthday) = 12 AND extract('day' from birthday) = 1;
+
+
+----6
+
+DELETE FROM users
+WHERE extract ('years' from age(birthday)) > 65;
+
+
+----- 7 
+
+UPDATE users
+SET weight = 80
+WHERE extract('years' from age(birthday)) BETWEEN 40 AND 50;
