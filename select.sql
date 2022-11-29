e8f55043067410cc4db7c94edecd04b479b8ae2b
freshcodeAlice <freshjs.ofte@gmail.com>
2022-11-29 09:56:14 +0200

    Task resolve

--- select.sql
+++ select.sql
@@ -254,3 +254,33 @@ WHERE ctu.user_id = 1;
 SELECT * FROM users
 LIMIT 15
 OFFSET 30;
+
+
+------------------------------
+
+
+SELECT id, concat(first_name, ' ', last_name) AS "full name", gender, email FROM users;
+
+
+/*
+Знайдіть всіх користувачів, повне ім'я (ім'я + прізвище) яких > 5 символів
+
+*/
+
+
+--v1
+SELECT id, 
+concat(first_name, ' ', last_name) AS "full name", 
+gender, email 
+FROM users
+WHERE char_length(concat(first_name, ' ', last_name)) > 5;
+
+
+--v2
+
+SELECT *
+FROM (SELECT id, 
+concat(first_name, ' ', last_name) AS "full name", 
+gender, email 
+FROM users) AS "FN"
+WHERE char_length("FN"."full name") > 5;
