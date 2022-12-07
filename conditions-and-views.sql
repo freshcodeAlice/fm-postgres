e66db08a29ff7b6384ee19b6d475497dce3383fb
freshcodeAlice <freshjs.ofte@gmail.com>
2022-12-07 11:37:12 +0200

    task resolve

--- conditions-and-views.sql
+++ conditions-and-views.sql
@@ -109,3 +109,61 @@ SELECT *, (
     END
 )
 FROM users;
+
+
+/*
+Вивести всі телефони (з таблиці products), 
+якщо ціна більше 6тис - флагман
+якщо ціна від 2 до 6 - середній клас
+якщо ціна <2 тис - бюджетний
+
+*/
+
+
+SELECT *,(
+    CASE
+    WHEN price > 6000 THEN 'Флагман'
+    WHEN price BETWEEN 2000 AND 6000 THEN 'Середній клас'
+    WHEN price < 2000 THEN 'Бюджетний'
+    ELSE 'Інше'
+END
+)
+FROM products;
+
+
+/*
+Вивести користувачів з інформацією про їхні замовлення у вигляді:
+- якщо більше >=3 - постійний клієнт
+- якщо від 1 до 2 - активний клієнт
+- якщо 0 замовлень - новий клієнт
+
+*/
+
+SELECT u.id, u.email, (
+    CASE 
+        WHEN count(o.id) >= 3
+        THEN 'Постійний клієнт'
+        WHEN count(o.id) BETWEEN 1 AND 2
+        THEN 'Активний клієнт'
+        WHEN count(o.id) = 0
+        THEN 'Новий клієнт'
+        ELSE 'Клієнт'
+    END
+)
+FROM users AS u
+LEFT JOIN orders AS o 
+ON u.id = o.customer_id
+GROUP BY u.id, u.email
+ORDER BY count(o.id);
+
+
+
+INSERT INTO users (
+    first_name,
+    last_name,
+    email,
+    gender,
+    is_subscribe,
+    birthday
+) VALUES
+('Iron', 'Man', 'tonymail@com', 'male', true, '1990-08-09');
